b53a530ba129278b45d845c72aa85487354182cd
Ajil Shaji <94738733+ajil02@users.noreply.github.com>
2023-12-28 10:17:25 +0530

    Update view_orders.dart

--- Pages/view_orders.dart
+++ Pages/view_orders.dart
@@ -1,4 +1,3 @@
-/*
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -7,7 +6,7 @@ class ViewOrders extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Admin Page'),
+        title: Text('Orders'),
       ),
       body: StreamBuilder<QuerySnapshot>(
         stream: FirebaseFirestore.instance
@@ -24,59 +23,10 @@ class ViewOrders extends StatelessWidget {
                 Map<String, dynamic> orderData =
                     orders[index].data() as Map<String, dynamic>;
 
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.all(8),
-                  child: ListTile(
-                    title: Text('Order ID: ${orderData['orderId']}'),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Items:'),
-                        for (var item in orderData['items'])
-                          ListTile(
-                            title: Text(item['itemName']),
-                            subtitle: Text('Quantity: ${item['quantity']}'),
-                          ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            );
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-    );
-  }
-}
-*/
-import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class ViewOrders extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Admin Page'),
-      ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance
-            .collection('Orders')
-            .orderBy('timestamp', descending: false)
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            List<DocumentSnapshot> orders = snapshot.data!.docs;
-
-            return ListView.builder(
-              itemCount: orders.length,
-              itemBuilder: (context, index) {
-                Map<String, dynamic> orderData =
-                    orders[index].data() as Map<String, dynamic>;
+                if (orderData['isHidden'] == true) {
+                  // Skip rendering if the item is hidden
+                  return Container();
+                }
 
                 return Card(
                   elevation: 5,
@@ -92,39 +42,50 @@ class ViewOrders extends StatelessWidget {
                         for (var item in orderData['items'])
                           ListTile(
                             title: Text(item['itemName']),
-                            subtitle: Text(
-                              'Price: \$${item['price'].toStringAsFixed(2)} | Quantity: ${item['quantity']} | Delivered: ${item['delivered'] ? 'Yes' : 'No'}',
-                            ),
-                            trailing: Container(
-                              color: item['delivered']
-                                  ? Colors.green[
-                                      300] // Set background color to green if delivered
-                                  : Colors.blue[
-                                      300], // Set background color to blue if not delivered
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  // Toggle delivered status when the button is pressed
-                                  markAsDelivered(
-                                    orderData['orderId'],
-                                    item['itemName'],
-                                    !item['delivered'],
-                                  );
-                                },
-                                child: Text(
-                                  item['delivered']
-                                      ? 'Undo Delivered'
-                                      : 'Mark Delivered',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                style: ElevatedButton.styleFrom(
-                                  primary: Colors.transparent,
-                                  shadowColor: Colors.transparent,
-                                ),
+                            subtitle: Text('Quantity: ${item['quantity']}'),
+                          ),
+                        SizedBox(height: 8),
+                        Container(
+                          color: orderData['delivered'] == true
+                              ? Colors.green[300]
+                              : Colors.blue[300],
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Toggle delivered status for the entire order
+                              markOrderAsDelivered(orderData['orderId'],
+                                  !orderData['delivered']);
+                            },
+                            child: Text(
+                              orderData['delivered'] == true
+                                  ? 'Delivered'
+                                  : 'Mark as Delivered',
+                              style: TextStyle(
+                                color: Colors.white,
                               ),
                             ),
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.transparent,
+                              shadowColor: Colors.transparent,
+                            ),
                           ),
+                        ),
+                        SizedBox(height: 8),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Hide the order from the display
+                            hideOrder(orders[index].reference);
+                          },
+                          child: Text(
+                            'Hide',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.grey,
+                            shadowColor: Colors.transparent,
+                          ),
+                        ),
                       ],
                     ),
                   ),
@@ -139,33 +100,39 @@ class ViewOrders extends StatelessWidget {
     );
   }
 
-  void markAsDelivered(String orderId, String itemName, bool delivered) {
-    FirebaseFirestore.instance
-        .collection('Orders')
-        .where('orderId', isEqualTo: orderId)
-        .get()
-        .then((QuerySnapshot querySnapshot) {
-      querySnapshot.docs.forEach((DocumentSnapshot document) {
-        // Get the document reference
-        DocumentReference docRef = document.reference;
+  Future<void> markOrderAsDelivered(String orderId, bool delivered) async {
+    try {
+      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+          .collection('Orders')
+          .where('orderId', isEqualTo: orderId)
+          .get();
 
-        // Update the items array to mark the specific item as delivered
-        List<dynamic> items = document['items'] ?? [];
-        List<Map<String, dynamic>> updatedItems =
-            List<Map<String, dynamic>>.from(items);
-
-        for (int i = 0; i < updatedItems.length; i++) {
-          if (updatedItems[i]['itemName'] == itemName) {
-            updatedItems[i]['delivered'] = delivered;
-            break;
-          }
-        }
-
-        // Update the document with the modified items array
-        docRef.update({'items': updatedItems});
-      });
-    }).catchError((error) {
+      if (querySnapshot.docs.isNotEmpty) {
+        querySnapshot.docs.first.reference
+            .update({'delivered': delivered})
+            .then((value) => print("Order marked as delivered"))
+            .catchError(
+                (error) => print("Failed to mark order as delivered: $error"));
+      } else {
+        print("Document not found for orderId: $orderId");
+        // Handle the case when the document is not found
+      }
+    } catch (error) {
       print("Error getting documents: $error");
+      // Handle the error or return a value if needed
+      // For example, you can return a Future.error(error) here
+    }
+  }
+
+  void hideOrder(DocumentReference orderReference) {
+    // Hide the order from the local display
+    orderReference.update({
+      'isHidden': true,
+    }).then((value) {
+      print("Order hidden from the page.");
+    }).catchError((error) {
+      print("Failed to hide order: $error");
+      // Handle the error or return a value if needed
     });
   }
 }
